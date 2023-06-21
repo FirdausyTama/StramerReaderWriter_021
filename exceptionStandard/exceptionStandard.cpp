87472be2a52f78c3467ec256caf40ebab0e5a401
FirdausyTama <atama.firdausy@gmail.com>
2023-06-21 15:53:49 +0700

    membuat awal program array mempunyai 3 elemen dan memanggil array ke 5

--- exceptionStandard/exceptionStandard.cpp
+++ exceptionStandard/exceptionStandard.cpp
@@ -4,3 +4,13 @@
 #include <array>
 //untuk obyek array yang akan kita gunakan
 using namespace std;
+int main()
+{
+	cout << "Awal program" << endl; //penanda 1:...
+	try {
+		array<int, 3> data = { 3, 5, 7 };
+		//pesan array integer 3 elemen
+		cout << data.at(5) << endl;
+		//memanggil array elemn ke 5
+	}
+}
