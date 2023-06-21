3727fed370e4566ed5b5270c65bda2058a3ff878
FirdausyTama <atama.firdausy@gmail.com>
2023-06-21 15:44:05 +0700

    menangkap dan akan di eksekusi

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -6,5 +6,10 @@ int main()
 	try {
 		cout << "Selamat Belajar di Prodi TI UMY" << endl;
 		throw 0, 5; //melemparkar sebuah interger maka
+		cout << "pertanyaan tidak akan dieksekusi " << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
 	}
 }
